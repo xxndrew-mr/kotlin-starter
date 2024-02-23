b96d7610de07d3fc5e0e743844ec8f0e542b3d77
Andre Marshandito <andremarshandito@gmail.com>
2024-02-24 04:27:40 +0700

    update oop

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,9 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "kotlin-starter"
-
+include("src:main:OOP")
+findProject(":src:main:OOP")?.name = "OOP"
+include("OOP")
+include("OOP:src:main:OOP")
+findProject(":OOP:src:main:OOP")?.name = "OOP"
+include("kotlin-oop")
