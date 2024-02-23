8e0ae5e2b26399daf1d0f9d2e326aa029c8df8b6
Andre Marshandito <andremarshandito@gmail.com>
2024-02-23 19:29:11 +0700

    update anatomi function

---
+++ src/main/kotlin/AnatomiFunction.kt
@@ -0,0 +1,20 @@
+package org.example
+
+//Pada dasarnya sebuah fungsi memiliki 2 (dua) bagian utama yaitu function header dan function body.
+
+//1.Function header adalah bagian yang merupakan konstruksi dari sebuah fungsi untuk menentukan perilakunya
+// akan seperti apa. Di dalam function header terdapat visibility modifier, kata kunci fun, nama, daftar parameter dan nilai kembalian dari fungsi tersebut.
+
+/*
+ private fun sum(valueA : Int, valueB : Int): Int
+ */
+
+//function body, yang ditandai dengan curly braces. Di dalamnya kita akan menempatkan sebuah logika kode baik itu sebuah expression atau statement.
+// kita wajib menambah sebuah baris kode yang diawali dengan kata kunci return yang diikuti oleh expression untuk menetapkan nilai yang akan dikembalikan.
+//
+//Sebaliknya, jika kita tidak ingin mengembalikan nilai, kita tidak perlu menambahkannya seperti yang disebutkan di atas.
+
+/*
+    return valueA + ValueB
+ */
+
