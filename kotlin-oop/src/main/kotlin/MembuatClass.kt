cd98a62d0fd65d3e51ead8a588583e8b7f1789bc
Andre Marshandito <andremarshandito@gmail.com>
2024-02-24 18:01:11 +0700

    update class

--- kotlin-oop/src/main/kotlin/MembuatClass.kt
+++ kotlin-oop/src/main/kotlin/MembuatClass.kt
@@ -1,2 +1,32 @@
 package org.example
 
+class Animal () {
+    var name : String = "Kucing"
+    var weight : Double = 3.2
+    var age : Int = 2
+    var isMammal : Boolean = true
+
+    fun eat(){
+        println("$name makan!")
+    }
+
+    fun sleep(){
+        println("$name tidur!")
+    }
+}
+
+fun main() {
+    val rumahCat = Animal()
+    println("Nama: ${rumahCat.name}), Berat: ${rumahCat.weight}, Umur : ${rumahCat.age}, Mamalia : ${rumahCat.isMammal}")
+    rumahCat.eat()
+    rumahCat.sleep()
+
+    //Mengubah Properti dari class rumahCat
+
+    rumahCat.name = "Kucing Putih"
+    rumahCat.weight = 6.0
+    rumahCat.age = 3
+    println("Nama: ${rumahCat.name}, Berat: ${rumahCat.weight}, Umur : ${rumahCat.age}, Mamalia : ${rumahCat.isMammal}")
+    rumahCat.eat()
+    rumahCat.sleep()
+}
