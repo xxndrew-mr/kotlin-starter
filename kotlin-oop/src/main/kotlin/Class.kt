af873927b9d29550dfd265b396e06d8b49a6ccc5
Andre Marshandito <andremarshandito@gmail.com>
2024-02-24 17:42:33 +0700

    update class

---
+++ kotlin-oop/src/main/kotlin/Class.kt
@@ -0,0 +1,21 @@
+package org.example
+
+//Setiap kelas memiliki atribut dan behaviour. Dalam Kotlin,
+// attributes lebih sering disebut dengan properties, sedangkan
+// behaviour atau fungsi yang di dalam class sering disebut method.
+
+/*
+
+
+
+             animal
+Properti                Method
+
+name: String            eat()
+                        sleep()
+weight: Double
+age: Int
+isMammal: Boolean
+
+
+ */
